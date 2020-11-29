8085182bede90880b9a7b7a795fc851e0403e3ba
unknown <carlosenmm@gmail.com>
2020-11-29 10:58:26 -0300

    alteracao do project e do core para o db

--- ingestor/project.clj
+++ ingestor/project.clj
@@ -5,7 +5,13 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.apache.kafka/kafka_2.12 "2.4.1"]
-                 [com.fzakaria/slf4j-timbre "0.3.17"]]
+                 [com.fzakaria/slf4j-timbre "0.3.17"]
+                 [com.datastax.cassandra/cassandra-driver-core "3.8.0"]
+                 [metosin/jsonista "0.2.5"]
+                 ;; [io.netty/netty "3.4.5.Final"]
+                 ;; [io.netty/netty "3.9.9.Final"]
+                 ;; [io.netty/netty-tcnative-boringssl-static "2.0.9.Final"]
+                 [io.netty/netty-transport-native-epoll "4.1.9.Final"]]
   :main ^:skip-aot ingestor.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- ingestor/src/ingestor/core.clj
+++ ingestor/src/ingestor/core.clj
@@ -1,5 +1,23 @@
 (ns ingestor.core
-  (:import [org.apache.kafka.clients.consumer KafkaConsumer]))
+  (:require [ingestor.db :as db])
+  (:import [com.datastax.driver.core Cluster ConsistencyLevel HostDistance PoolingOptions QueryOptions]
+           [com.datastax.driver.core.policies LatencyAwarePolicy Policies]
+           [org.apache.kafka.clients.consumer KafkaConsumer]))
+
+(defn configs-cassandra [builder quorum?]
+  (-> builder
+      (.addContactPoint "host.docker.internal")
+      (.withCredentials "cassandra" "cassandra")
+      (.withLoadBalancingPolicy (.build (LatencyAwarePolicy/builder (Policies/defaultLoadBalancingPolicy))))
+      (.withPoolingOptions (-> (PoolingOptions.)
+                               (.setMaxQueueSize 1024)
+                               (.setMaxRequestsPerConnection HostDistance/LOCAL 2048)))
+      (.withQueryOptions (-> (QueryOptions.)
+                             (.setFetchSize 2000)
+                             (.setConsistencyLevel (if quorum?
+                                                     ConsistencyLevel/QUORUM
+                                                     ConsistencyLevel/ONE))
+                             (.setDefaultIdempotence true)))))
 
 (defn new-consumer [group-id]
   (KafkaConsumer.
@@ -11,13 +29,23 @@
     "auto.offset.reset"                     "earliest"}))
 
 (defn -main [& args]
-  (let [interface (first args)]
+  (let [interface (first args)
+        cluster (-> (Cluster/builder)
+                    (configs-cassandra false)
+                    (.build))
+        session (.connect cluster "ingestor")]
     (case interface
       "consumer" (let [grupo-de-consumo "1" ;(str (rand))
                        consumer (new-consumer grupo-de-consumo)]
                    (println "Começando a leitura das mensagens")
                    (.subscribe consumer ["documents"])
-                   (doseq [i (range 1 5)] ; while true
+                   (while true ; doseq [i (range 1 5)]
                      (let [records (.poll consumer (java.time.Duration/ofMillis 5000))]
                        (doseq [record records]
-                         (println (format "offset = %s\nkey = %s\nvalue = %s\n" (.offset record) (.key record) (.value record))))))))))
+                         (println
+                          (format "Fazendo insert no BD do comando:\noffset = %s\nkey = %s\nvalue = %s\npartition = %s\n"
+                                  (.offset record)
+                                  (.key record)
+                                  (.value record)
+                                  (.partition record)))
+                         (db/upsert-cmd (.value record) session (.offset record)))))))))
